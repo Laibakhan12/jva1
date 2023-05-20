8eebeeb2e1166dbd664e72fbe01c1afd104ec9ad
Laibakhan12 <128514647+Laibakhan12@users.noreply.github.com>
2023-05-20 13:37:30 -0700

    Add files via upload

---
+++ JS Assignment/js assignment 3/app.js
@@ -0,0 +1,59 @@
+//  let data1="Hello";
+//  let data2 = " World";
+
+// let data3 = data1 + data2; 
+// console.log(data3);
+
+// contanenation--------------------------------------------------------------
+
+
+//  let data1= 5;
+//  let data2 =  2;
+
+//  let data3 = data1 + data2; //string concatenation
+//  console.log(data3);
+
+
+// Data.include---------------------------------------------------
+
+// let data="hello";
+// let data2 = data.includes("h");
+// console.log(data2);
+
+// ---------string is also add in numbers ----------------
+// by using 
+// (+)
+// (parseInt)
+// (Number)
+
+
+// let data = "6";
+// let data2 = 3;
+// console.log(parseInt(data)+ data2)
+
+//-------------------------Math operators-------------------------------
+
+// let data1 = 8;
+// let data2 = 2;
+// console.log( data1 * data2 )
+
+//--------------prompt---------------------------------------------
+
+// let userFavNum = prompt("What is your favourite number?");
+// console.log(userFavNum)
+
+//random or round---------------------------------------
+
+// lumber*10;et randomNumber = Math.random();
+// randomNumber = randomN
+// randomNumber *=10 //shorthand
+// console.log(roundedNumber)
+
+// let num = 5 ** 5;
+// console.log(num)
+
+// let userName = prompt("enter your name");
+// console.log(userName);
+
+let userAge = prompt("enter your age");
+document.write(userAge);
